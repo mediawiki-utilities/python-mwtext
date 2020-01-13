04a6fab7bc36929098f4deaca2ad108216926c41
halfak <aaron.halfaker@gmail.com>
2020-01-13 15:07:58 -0600

    Adds __version__ to base module.

--- mwtext/__init__.py
+++ mwtext/__init__.py
@@ -1,3 +1,7 @@
 from .wikitext_preprocessor import WikitextPreprocessor
+from .about import (__name__, __version__, __author__, __author_email__,
+                    __description__, __license__, __url__)
 
-__all__ = (WikitextPreprocessor, )
+__all__ = (WikitextPreprocessor,
+           __name__, __version__, __author__, __author_email__,
+           __description__, __license__, __url__)
