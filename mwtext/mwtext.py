beece27379c28ee5e5c56af9e5dac01be6410ac7
halfak <aaron.halfaker@gmail.com>
2020-01-14 17:22:23 -0600

    Adds learn_vectors utility.

--- mwtext/mwtext.py
+++ mwtext/mwtext.py
@@ -4,7 +4,9 @@ router = mwcli.Router(
     "mwtext",
     "This script provides access to a set of utilities for text processing",
     {'preprocess_text': "Converts an XML dump to preprocessed plaintext. " +
-                        "One line per chunk."}
+                        "One line per chunk.",
+     'learn_vectors':   "Learn a set of word vectors from preprocessed " +
+                        "plaintext"}
 )
 
 main = router.main
---
+++ mwtext/utilities/learn_vectors.py
@@ -0,0 +1,53 @@
+"""
+Trains embeddings using preprocessed text and labels using fasttext.  Outputs
+a complete set of vectors for the words observed in the input text.
+
+Usage:
+    learn_vectors (-h|--help)
+    learn_vectors <input> [--param=<kv>]... [--output=<path>]
+
+Options:
+    --param=<kv>     A named parameter for training the fasttext model.  The
+                     value will be interpretted as a JSON blob.
+    <input>          The path of an input file containing labels and words in
+                     the fasttext format.
+    --output=<path>  The output file to write vectors to [default: <stdout>]
+    --debug          Print debug information.
+"""
+import json
+import sys
+
+import docopt
+import fasttext
+
+
+def main(argv):
+    args = docopt.docopt(__doc__, argv=argv)
+
+    input_path = args['<input>']
+
+    params = {}
+    for key_value in args['--param']:
+        key, value_str = key_value.split("=", 1)
+        value = json.loads(value_str)
+        params[key] = value
+
+    if args['--output'] == "<stdout>":
+        output = sys.stdout
+    else:
+        output = open(args['--output'], "w")
+
+    learn_vectors(input_path, params, output)
+
+
+def learn_vectors(input_path, params, output):
+    model = fasttext.train_supervised(input_path, **params)
+
+    dimensions = params.get('dim', 100)
+    words = len(model.words)
+
+    output.write("{0} {1}\n".format(words, dimensions))
+    for word in model.get_words():
+        vector = model.get_word_vector(word)
+        output.write(" ".join([word] + (str(v) for v in vector)))
+        output.write("\n")
