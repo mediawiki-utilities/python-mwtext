efaa8c96572b4526b54ba495f2d74e146636bfe9
Gabriel <gabriel.altay@gmail.com>
2020-05-10 17:29:21 -0400

    more docstrings, more kwargs, code reorg, type hints

--- mwtext/wikitext_preprocessor_mwpfh.py
+++ mwtext/wikitext_preprocessor_mwpfh.py
@@ -11,6 +11,7 @@ paragraph objects is heavily inspired by Wikipedia2Vec
  * https://github.com/wikipedia2vec/wikipedia2vec
 
 """
+from typing import Callable, Iterable
 import mwparserfromhell
 
 
@@ -26,24 +27,52 @@ DEFAULT_FORBIDDEN_SECTIONS = [
 ]
 
 DEFAULT_FORBIDDEN_LINK_PREFIXES = [
+    "#",       # self links
+    ":",       # used mostly for image, category, or interlanguage links
     "file:",
     "image:",
+]
+
+DEFAULT_ALLOWED_TAGS = [
+    "b",
+    "i",
+    "u",
 ]
 
 
 class WikitextPreprocessorMwpfh:
 
+    """Wikitext Preprocessor using MediaWiki Parser From Hell
+
+    Args:
+        forbidden_link_prefixes (Iterable[str]): ignore links with these
+            (case insensitive) prefixes.
+        forbidden_sections (Iterable[str]): skip sections with these
+            (case insensitive) titles.
+        allowed_tags (Iterable[str]): include the contents of these tags
+        include_disallowed_tag_tokens (bool): if True, include a single
+            token for tags that are not in the `allowed_tags` list.  For
+            example a <table> token to represent all the markup of a table.
+        preprocess_func (Callable[[str], str]): arbitary callable that will
+            be called on all the test pieces included in the output.
+        canonicalize_link_targets (bool): if True, uppercase the first character
+            of link targets and replace spaces with underscores
+    """
     def __init__(
         self,
-        forbidden_link_prefixes=DEFAULT_FORBIDDEN_LINK_PREFIXES,
-        forbidden_sections=DEFAULT_FORBIDDEN_SECTIONS,
-        include_tag_tokens=False,
-        preprocess_func=lambda x: x,
+        forbidden_link_prefixes: Iterable[str] = DEFAULT_FORBIDDEN_LINK_PREFIXES,
+        forbidden_sections: Iterable[str] = DEFAULT_FORBIDDEN_SECTIONS,
+        allowed_tags: Iterable[str] = DEFAULT_ALLOWED_TAGS,
+        include_disallowed_tag_tokens: bool = False,
+        preprocess_func: Callable[[str], str] = lambda x: x,
+        canonicalize_link_targets: bool = False
     ):
         self.forbidden_link_prefixes = forbidden_link_prefixes
         self.forbidden_sections = forbidden_sections
-        self.include_tag_tokens = include_tag_tokens
+        self.allowed_tags = allowed_tags
+        self.include_disallowed_tag_tokens = include_disallowed_tag_tokens
         self.preprocess_func = preprocess_func
+        self.canonicalize_link_targets = canonicalize_link_targets
         self._reset()
 
     def _reset(self):
@@ -53,7 +82,12 @@ class WikitextPreprocessorMwpfh:
         self._current_text = ""
         self._current_links = []
 
-    def _parse_heading_node(self, node):
+    def _parse_heading_node(self, node: mwparserfromhell.nodes.Heading) -> None:
+        """Parse heading node.
+
+        If this is a level 2 node (== heading ==), set the section name and decide
+        if we are skipping this section.
+        """
         if node.level == 2:
             self._section_name = node.title.strip_code().strip()
             if self._section_name.lower() in self.forbidden_sections:
@@ -62,67 +96,86 @@ class WikitextPreprocessorMwpfh:
                 self._section_idx += 1
                 self._skipping_section = False
 
-    def _parse_tag_node(self, node):
+    def _parse_tag_node(self, node: mwparserfromhell.nodes.Tag) -> None:
+        """Parse tag node.
 
-        if not node.contents:
+        For allowed tags, include the contents of the tag in the text stream.
+        For disallowed tags, optionally include a token to indicate the presence
+        of the tag but not the contents (e.g. include a single <table> token in the
+        text stream but nothing from the table markdown itself).
+        """
+        # never include tags with no content
+        if len(node.contents) == 0:
             return
 
-        if node.tag in ('b', 'i', 'u'):
-            text = node.contents.strip_code()
+        # add the contents of allowed tags to the text stream
+        if node.tag in self.allowed_tags:
+            text = node.contents.strip_code().strip()
             self._current_text += self.preprocess_func(text)
+
+        # optionally add a single token for disallowed tags
         else:
-            if self.include_tag_tokens:
+            if self.include_disallowed_tag_tokens:
                 text = "<{}>".format(node.tag.strip_code().strip())
                 self._current_text += self.preprocess_func(text)
 
-    def _parse_external_link_node(self, node):
-        if not node.title:
+    def _parse_external_link_node(self, node: mwparserfromhell.nodes.ExternalLink):
+        """Parse external link node.
+
+        Include the title of external links in the text stream.
+        """
+        if node.title is None:
             return
-        text = node.title.strip_code()
+        text = node.title.strip_code().strip()
         self._current_text += self.preprocess_func(text)
 
-    def _parse_wikilink_node(self, node):
-        """docs on link structure: https://en.wikipedia.org/wiki/Help:Link"""
-        title = node.title.strip_code().strip()
+    def _parse_wikilink_node(self, node: mwparserfromhell.nodes.Wikilink):
+        """Parse wikilink nodes.
 
-        # skip self links
-        if title.startswith("#"):
+        basic cases:,
+          [[target_page]]
+            node.title = "target_page", node.text = None
+          [[target_page|anchor_text]]
+            node.title = "target_page", node.text = "anchor_text"
+
+        docs on link structure: https://en.wikipedia.org/wiki/Help:Link
+        """
+        target = node.title.strip_code().strip()
+
+        if len(target) == 0:
             return
 
-        if title.startswith(':'):
-            title = title[1:]
-
-        if not title:
+        if any([
+            target.lower().startswith(prefix)
+            for prefix in self.forbidden_link_prefixes
+        ]):
             return
 
-        # normalize
-        title = title[0].upper() + title[1:].replace(' ', '_')
+        # remove section specific component
+        target = target[:target.index("#")] if "#" in target else target
 
-        # remove links to page sections
-        title = title[:title.index("#")] if "#" in title else title
+        # normalize (TODO: check the format the SQL dumps use)
+        if self.canonicalize_link_targets:
+            target = target[0].upper() + target[1:].replace(" ", "_")
 
-        if node.text:
-            text = node.text.strip_code()
-            # e.g. https://en.wikipedia.org/wiki/Wikipedia:Extended_image_syntax
-            if any([
-                title.lower().startswith(prefix)
-                for prefix in self.forbidden_link_prefixes
-            ]):
-                return
-        else:
-            text = node.title.strip_code()
+        # get anchor text
+        anchor = node.text.strip_code().strip() if node.text is not None else target
 
-        text = self.preprocess_func(text)
+        # package up link
+        anchor = self.preprocess_func(anchor)
         start = len(self._current_text)
-        self._current_text += text
+        self._current_text += anchor
         end = len(self._current_text)
-        self._current_links.append((title, text, start, end))
+        self._current_links.append((target, anchor, start, end))
 
-    def _parse_text_node(self, node):
+    def _parse_text_node(self, node: mwparserfromhell.nodes.Text):
+        """Parse text node.
 
+        Create a list of paragraph objects from a text node.
+        """
         paragraphs_local = []
-        for (n, text) in enumerate(node.split('\n')):
-            if n == 0:
+        for (ii, text) in enumerate(node.split("\n")):
+            if ii == 0:
                 self._current_text += self.preprocess_func(text)
             else:
                 if self._current_text and not self._current_text.isspace():
@@ -137,8 +190,11 @@ class WikitextPreprocessorMwpfh:
 
         return paragraphs_local
 
-    def process(self, text):
+    def process(self, text: str):
+        """Process wikitext markup into a list of paragraph objects.
 
+        TODO: Describe paragraph object format when finalized.
+        """
         self._reset()
         wikicode = mwparserfromhell.parse(text)
         paragraphs = []
