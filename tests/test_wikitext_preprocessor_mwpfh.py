83204d621c3cd1e9a6230e0fc7e1963431f15f7d
Gabriel Altay <gabriel.altay@gmail.com>
2020-05-11 12:06:16 -0400

    reorganized test fixtures

--- tests/test_wikitext_preprocessor_mwpfh.py
+++ tests/test_wikitext_preprocessor_mwpfh.py
@@ -6,57 +6,75 @@ Inspiration for test cases drawn from,
 from mwtext import WikitextPreprocessorMwpfh
 
 WIKILINK_TEST_FIXTURES = {
-    "basic": "[[target_title]]",
-    "rename": "[[target_title|anchor_text]]",
-    "kingdom": "[[kingdom (biology)]]",
-    "seattle": "[[Seattle, Washington]]",
-    "village": "[[Wikipedia:Village pump]]",
-    "style": "[[Wikipedia:Manual of Style#Links]]",
-    "kingdom|": "[[kingdom (biology)|]]",
-    "seattle|": "[[Seattle, Washington|]]",
-    "village|": "[[Wikipedia:Village pump|]]",
-    "style|": "[[Wikipedia:Manual of Style#Links|]]",
-    "#self": "[[#self]]",
+    "basic": {
+        "wikitext": "[[target_title]]",
+        "text": "target_title",
+        "links": [("target_title", "target_title", 0, 12)],
+    },
+    "rename": {
+        "wikitext": "[[target_title|anchor_text]]",
+        "text": "anchor_text",
+        "links": [("target_title", "anchor_text", 0, 11)],
+    },
+    "kingdom": {
+        "wikitext": "[[kingdom (biology)]]",
+        "text": "kingdom (biology)",
+        "links": [("kingdom (biology)", "kingdom (biology)", 0, 17)],
+    },
+    "seattle": {
+        "wikitext": "[[Seattle, Washington]]",
+        "text": "Seattle, Washington",
+        "links": [("Seattle, Washington", "Seattle, Washington", 0, 19)],
+    },
+    "village": {
+        "wikitext": "[[Wikipedia:Village pump]]",
+        "text": "Village pump",
+        "links": [('Wikipedia:Village pump', 'Village pump', 0, 12)],
+    },
+    "style": {
+        "wikitext": "[[Wikipedia:Manual of Style#Links]]",
+        "text": "Manual of Style",
+        "links": [('Wikipedia:Manual of Style', 'Manual of Style', 0, 15)],
+    },
+    "kingdom|": {
+        "wikitext": "[[kingdom (biology)|]]",
+        "text": "kingdom (biology)",
+        "links": [("kingdom (biology)", "kingdom (biology)", 0, 17)],
+    },
+    "seattle|": {
+        "wikitext": "[[Seattle, Washington|]]",
+        "text": "Seattle, Washington",
+        "links": [("Seattle, Washington", "Seattle, Washington", 0, 19)],
+    },
+    "village|": {
+        "wikitext": "[[Wikipedia:Village pump|]]",
+        "text": "Village pump",
+        "links": [('Wikipedia:Village pump', 'Village pump', 0, 12)],
+    },
+    "style|": {
+        "wikitext": "[[Wikipedia:Manual of Style#Links|]]",
+        "text": "Manual of Style",
+        "links": [('Wikipedia:Manual of Style', 'Manual of Style', 0, 15)],
+    },
+    "#self": {
+        "wikitext": "[[#self]]",
+        "text": "self",
+        "links": [('#self', 'self', 0, 4)],
+    },
 }
 
-WIKILINK_EXPECTED_TEXT = {
-    "basic": "target_title",
-    "rename": "anchor_text",
-    "kingdom": "kingdom (biology)",
-    "seattle": "Seattle, Washington",
-    "village": "Village pump",
-    "style": "Manual of Style",
-    "kingdom|": "kingdom (biology)",
-    "seattle|": "Seattle, Washington",
-    "village|": "Village pump",
-    "style|": "Manual of Style",
-    "#self": "self",
-}
-
-WIKILINK_EXPECTED_LINKS = {
-    "basic": [("target_title", "target_title", 0, 12)],
-    "rename": [("target_title", "anchor_text", 0, 11)],
-    "kingdom": [("kingdom (biology)", "kingdom (biology)", 0, 17)],
-    "seattle": [("Seattle, Washington", "Seattle, Washington", 0, 19)],
-    "village": [('Wikipedia:Village pump', 'Village pump', 0, 12)],
-    "style": [('Wikipedia:Manual of Style', 'Manual of Style', 0, 15)],
-    "kingdom|": [("kingdom (biology)", "kingdom (biology)", 0, 17)],
-    "seattle|": [("Seattle, Washington", "Seattle, Washington", 0, 19)],
-    "village|": [('Wikipedia:Village pump', 'Village pump', 0, 12)],
-    "style|": [('Wikipedia:Manual of Style', 'Manual of Style', 0, 15)],
-    "#self": [('#self', 'self', 0, 4)],
-}
 
 
 def test_wikilinks_only():
-    for fixname in WIKILINK_TEST_FIXTURES:
+    for test_name, test_data in WIKILINK_TEST_FIXTURES.items():
         wtpp = WikitextPreprocessorMwpfh()
-        text = WIKILINK_TEST_FIXTURES[fixname]
-        parsed = wtpp.process(text)
+        parsed = wtpp.process(test_data["wikitext"])
         expected = [
-            {"section_idx": 0,
-             "section_name": "Introduction",
-             "text": WIKILINK_EXPECTED_TEXT[fixname],
-             "wikilinks": WIKILINK_EXPECTED_LINKS[fixname]}
+            {
+                "section_idx": 0,
+                "section_name": "Introduction",
+                "text": test_data["text"],
+                "wikilinks": test_data["links"]
+            },
         ]
         assert parsed == expected
