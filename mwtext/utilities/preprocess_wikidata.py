2fd62d0bdd3327507758f9d114d148b69112e0f8
dibyaaaaax <dibya.gautam7@gmail.com>
2020-06-06 13:39:55 +0530

    standarize preprocess_wikidata

--- mwtext/utilities/preprocess_wikidata.py
+++ mwtext/utilities/preprocess_wikidata.py
@@ -1,9 +1,37 @@
+r"""
+``$ mwtext preprocess_wikidata -h``
+::
+    Converts WIkidata JSON dumps to string of property/value pairs.
+    Usage:
+        preprocess_wikidata (-h|--help)
+        preprocess_wikidata [<input-file>...]
+                            [--threads=<num>] [--output=<path>]
+                            [--compress=<type>] [--verbose] [--debug]
+    Options:
+        -h|--help           Print this documentation
+        <input-file>        The path to a Wikidata JSON Dump file
+                            [default: <stdin>]
+        --threads=<num>     If a collection of files are provided, how many
+                            processor threads? [default: <cpu_count>]
+        --output=<path>     Write output to a directory with one output file
+                            per input path.  [default: <stdout>]
+        --compress=<type>   If set, output written to the output-dir will be
+                            compressed in this format. [default: bz2]
+        --verbose           Print progress information to stderr.  Kind of a
+                            mess when running multi-threaded.
+        --debug             Print debug logs.
+"""
+
 import bz2
 import json
 import os
 import random
+import sys
 
 import mwbase
+import mwcli
+
+from ..wikidata_preprocessor import WikidataPreprocessor
 
 
 def getPropertyValue(entity):
@@ -34,25 +62,40 @@ def isRelevantEntity(entity):
     return (len(sitelinks) > 0)
 
 
-def preprocess_wikidata(WIKIDATA_DIR):
-    with bz2.open(os.path.join(WIKIDATA_DIR, 'latest-all.json.bz2'), 'rt') as fin:
-        next(fin)
-        for idx, line in enumerate(fin, start=1):
-            try:
-                item_json = json.loads(line)
-            except Exception:
-                try:
-                    item_json = json.loads(line[:-2])
-                except Exception:
-                    print("Error:", idx)
-                    continue
+def preprocess_wikidata(dump):
+    wikidata_preprocessor = WikidataPreprocessor()
+    for item_json in dump:
+        qid = item_json.get('id', None)
+        if not qid:
+            continue
 
-            qid = item_json.get('id', None)
-            if not qid:
-                continue
+        if verbose:
+            sys.stderr.write(qid + ": ")
+            sys.stderr.flush()
 
-            entity = mwbase.Entity.from_json(item_json)
-            if isRelevantEntity(entity):
-                prop_val = getPropertyValue(entity)
-                yield qid + ": " + prop_val
+        entity = mwbase.Entity.from_json(item_json)
+        if isRelevantEntity(entity):
+            prop_val = getPropertyValue(entity)
+            yield qid + ": " + prop_val
+            if verbose:
+                sys.stderr.write(".")
+                sys.stderr.flush()
+        else:
+            if verbose:
+                sys.stderr.write("-")
+                sys.stderr.flush()
+  
+        if verbose:
+            sys.stderr.write("\n")
+            sys.stderr.flush()
 
+
+streamer = mwcli.Streamer(
+    __doc__,
+    __name__,
+    preprocess_wikidata,
+    file_reader=mwcli.Streamer.read_json,
+    line_writer=mwcli.Streamer.write_line
+)
+
+main = streamer.main
---
+++ mwtext/wikidata_preprocessor.py
@@ -0,0 +1,2 @@
+class WikidataPreprocessor:
+	def process(self, text):
