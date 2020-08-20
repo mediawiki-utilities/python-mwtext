66c9e8980a5ac5f1cc92bcde379247eafdd19ce5
Aaron Halfaker <aaron.halfaker@gmail.com>
2020-08-20 16:14:16 -0500

    Increment version to 0.2.2
    Adds support for Wikidata item processing.

--- mwtext/about.py
+++ mwtext/about.py
@@ -1,5 +1,5 @@
 __name__ = "mwtext"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __author__ = "Aaron Halfaker"
 __author_email__ = "aaron.halfaker@gmail.com"
 __description__ = "A set of utilities for processing MediaWiki text."
